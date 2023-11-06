4d21a199d1cabb018f38329f1426a1f16bda5242
kaneraZh <ari1337ferrada@gmail.com>
2023-11-05 21:47:57 -0300

    door scene
    door scene code, no interactions implemented yet

--- scripts/Door.cs
+++ scripts/Door.cs
@@ -1,43 +1,57 @@
 using Godot;
-using System;
 
+[Tool]
 public partial class Door : Node3D
 {
 	[Export]
 	private PackedScene open;
 	[Export]
 	private PackedScene close;
+	private Node3D mesh;
+	public void set_mesh(Node3D value){
+		if(IsInstanceValid(mesh)) mesh.QueueFree();
+		mesh = value;
+		AddChild( value );
+	}
 	
 	public enum State
 	{
 		Open,
-		Closed
+		Close
 	}
-	/*
-	private State current_state {}
-	[Export]
-	private State start_as {
-		get { return start_as; }
-		set {
-			if(value==State.Open){
-				
+	private State state_current;
+	public void set_state_current(State value){
+		//if(state_current == value) return;
+		switch(value)
+			{
+				case State.Open:
+					if(!IsInstanceValid(open)){
+						GD.PushError("No <open> PackedScene loaded");
+						return;
+					}
+					set_mesh( open.Instantiate<StaticBody3D>() );
+					break;
+				case State.Close:
+					if(!IsInstanceValid(close)){
+						GD.PushError("No <close> PackedScene loaded");
+						return;
+					}
+					set_mesh( close.Instantiate<StaticBody3D>() );
+					break;
 			}
-		}
+			state_current = value;
 	}
+
+	[Export]
+	private State state_start{ get; set; }
 	
 	[Export(PropertyHint.Flags, "open, close")]
 	private int actions;
-	*/
-	
-	
-	
-	// Called when the node enters the scene tree for the first time.
+	public bool can_open(){ return (actions&1)==1; }
+	public bool can_close(){ return ((actions>>1)&1)==1; }
+
 	public override void _Ready()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		set_state_current(state_start);
 	}
 }
